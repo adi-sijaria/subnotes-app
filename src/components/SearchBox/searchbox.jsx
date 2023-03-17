d1702e39c71fbe6ae5580c5fff0f96afee4cfa4a
adi-sijaria <adi.sijaria@gmail.com>
2023-03-18 04:24:21 +0530

    my changes

--- src/components/SearchBox/searchbox.jsx
+++ src/components/SearchBox/searchbox.jsx
@@ -14,7 +14,7 @@ const Searchbox = () => {
   return (
     <div className='search-box-container'>
       <input type="text" value={Text} className='search-box' placeholder='Enter your notes heading' onChange={(event) => setTextvalue(event.target.value)}/>
-      <IoMdAddCircle size={30} color={"blue"} onClick={()=>addTonotes(Text)} />
+      <IoMdAddCircle color='white' size={40} className='addnote-button'  onClick={()=>addTonotes(Text)} />
     </div>
   )
 }
--- src/components/SubnoteInput/SubnoteInput.jsx
+++ src/components/SubnoteInput/SubnoteInput.jsx
@@ -14,8 +14,8 @@ const SubnoteInput = ({notes}) => {
   }
   return (
          <div className='input-box-container'>
-      <input type="text" value={Text} className='input' placeholder='ADD SUBNOTES' onChange={(event) => setTextvalue(event.target.value)}/>
-      <IoMdAddCircle size={30} color={"blue"} onClick={()=>addtosubnotes({text:Text,notes:notes})} />
+      <input type="text" value={Text} className='input' placeholder='Add Subnotes' onChange={(event) => setTextvalue(event.target.value)}/>
+      <IoMdAddCircle className='addsubnote-button' size={35} color='white' onClick={()=>addtosubnotes({text:Text,notes:notes})} />
     </div>
   )
 }
